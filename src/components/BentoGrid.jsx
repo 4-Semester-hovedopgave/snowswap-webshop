4cab5ec7184b2e67c8fbff2c6dc7274384504fe0
Anarikke <anla0008@stud.kea.dk>
2025-12-08 08:52:56 +0100

    test

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -25,7 +25,7 @@ export default function BentoGrid() {
                         <Link href="/products">
 
                             <Image
-                                src="https://images.unsplash.com/photo-1488410766174-4d3ab8efd321?q=80&w=1734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                src="/explore.png"
                                 alt="Explore"
                                 fill
                                 className="object-cover"
