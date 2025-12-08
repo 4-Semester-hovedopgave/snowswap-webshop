4c1e610b2daa483e999a736d3b07ab52c205c623
Anarikke <anla0008@stud.kea.dk>
2025-12-08 09:34:41 +0100

    opdateret af next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.[^/]+$).*)"],
 };
 
 export default nextConfig;
